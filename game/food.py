0862244f26c3cb3d0797c7d85317b33205adf2d1
Siddarth Naik <siddarthnaik7@gmail.com>
2024-07-08 12:26:13 +0530

    Add refresh method to Food class

--- game/food.py
+++ game/food.py
@@ -9,4 +9,7 @@ class Food(Turtle) :
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("yellow")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         self.goto(x=random.randint(-260,260), y=random.randint(-260,260))
