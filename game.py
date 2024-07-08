1bbb67a822ea663d076cafdc415dfaaca40245a5
Siddarth Naik <siddarthnaik7@gmail.com>
2024-07-08 10:59:36 +0530

    Add snake movement

---
+++ game.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+from time import sleep
+from snake import Snake
+
+screen = Screen()
+screen.setup(height = 600, width = 600)
+screen.title("Snake game")
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+game_is_on = True
+
+screen.listen()
+screen.onkey(key="Up", fun=snake.Up)
+screen.onkey(key="Down", fun=snake.Down)
+screen.onkey(key="Left", fun=snake.Left)
+screen.onkey(key="Right", fun=snake.Right)
+
+while game_is_on:
+    screen.update()
+    sleep(0.2)
+    snake.move()
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,41 @@
+from turtle import Turtle
+STARTING_POSITION = [(0, 0),(-20, 0),(-40,0)]
+MOVE_DISTANCE = 20
+
+class Snake():
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for pos in STARTING_POSITION: 
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(pos)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range( len(self.segments) - 1, 0, -1):
+            x = self.segments[seg_num-1].xcor()
+            y = self.segments[seg_num-1].ycor()
+            self.segments[seg_num].goto(x, y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def Up(self):  
+        if self.head.heading() != 270.0:
+            self.head.setheading(90)
+
+    def Down(self):
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+        
+
+    def Left(self):
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
+    def Right(self):
+        if self.head.heading() != 180:
+            self.head.setheading(0)
