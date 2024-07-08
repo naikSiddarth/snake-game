de8886b773664885cd4a8a2b331539eb5be27fd2
Siddarth Naik <siddarthnaik7@gmail.com>
2024-07-08 13:25:34 +0530

    Add method to update score in Scoreboard class

--- game/scoreboard.py
+++ game/scoreboard.py
@@ -1,8 +1,17 @@
 from turtle import Turtle
-
+FONT = ("Arial", 8, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.write(f"Score : {self.score}", align="center", font=("Arial", 8, "normal"))
+        self.penup()
+        self.color("white")
+        self.hideturtle()
+        self.goto(0,280)
+        self.write(f"Score : {self.score}", align="center", font=FONT)
+        
+    def update(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score : {self.score}", align="center", font= FONT)
         
