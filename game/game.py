b4a4e49405c5c5fa8ca3b670fc3296fadff080d9
Siddarth Naik <siddarthnaik7@gmail.com>
2024-07-08 14:37:01 +0530

    Fixed bug in collision with tail

--- game/game.py
+++ game/game.py
@@ -41,7 +41,10 @@ while game_is_on:
 
     #Detect collision with tail 
     for segment in snake.segments:
-        if snake.head.distance(segment) < 10  :
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10  :
             game_is_on = False
             scoreboard.gameover()
+
 screen.exitonclick()
