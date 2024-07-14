6fa1c8d672234c390ef673d4835800a28441c8c6
Siddarth Naik <siddarthnaik7@gmail.com>
2024-07-14 09:19:45 +0530

    Add code track highscore

--- game/scoreboard.py
+++ game/scoreboard.py
@@ -4,17 +4,22 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.penup()
         self.color("white")
         self.hideturtle()
         self.goto(0,280)
-        self.write(f"Score : {self.score}", align="center", font=FONT)
-        
-    def update(self):
-        self.score += 1
-        self.clear()
-        self.write(f"Score : {self.score}", align="center", font= FONT)
+        self.write(f"Score : {self.score} , High score : {self.highscore}", align="center", font=FONT)
 
-    def gameover(self):
-        self.goto(0, 0)
-        self.write("Game Over", align="center", font=("Arial",20,"bold"))
+    def update(self):
+        self.clear()
+        self.write(f"Score : {self.score} High score : {self.highscore}", align="center", font= FONT)
+
+    def reset(self):
+        if self.score > self.highscore :
+            self.highscore = self.score
+        self.score = 0
+        self.update()
+    
+    def increase_score(self):
+        self.score += 1
